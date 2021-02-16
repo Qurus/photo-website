c8bbbadd2498547245a260c2e927d730595a2999
ali uen <flashnukers@gmail.com>
2021-02-16 18:38:39 +0100

    photo website first initial setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,37 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from 'gatsby'
+import Img from "gatsby-image"
+import './index.css'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const IndexPage = ({ data }) => {
+  return <div>
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <h1>Merhaba</h1>
+    <div className="photos">
+      {data.allFile.edges.map(({node}) => {
+      return <Img key={node.id} fluid={node.childImageSharp.fluid} />
+    })}
+  </div>
+    
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+  
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query IndexPageQuery {
+    allFile(filter: {absolutePath: {regex: "//photos//"}}) {
+    edges {
+      node {
+        id
+        childImageSharp {
+          fluid{
+          ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  }
+  }
+`
